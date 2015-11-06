2ce22b37e4aec743924f59a6022f10d9598ab4cf
Jason Campbell <jasoncampbell@google.com>
2015-11-06 01:27:02 -0800

    Working ndjson log

--- example/simple.js
+++ example/simple.js
@@ -4,7 +4,7 @@ var assert = require('assert')
 bench('for loop', 2000, function(done) {
   var original = [ 1, 2, 3, 4 ]
   var clone = []
-  var length = array.length;
+  var length = original.length;
   for (var i = 0; i < length; i++) {
     clone.push(original[i])
   }
--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const suite = require('./lib/suite')
 const benchmark = require('./lib/benchmark')
 const pump = require('pump')
 
-const lazy = thunky(function createHarness(callback) {
-  callback(suite())
+const through = require('through2')
+
+const setup = thunky(function createHarness(callback) {
+  process.nextTick(function next() {
+    var stream = suite()
+    pump(stream, through.obj(function(obj, enc, cb) {
+      cb(null, JSON.stringify(obj) + '\n')
+    }), process.stdout)
+    callback(stream)
+  })
 })
 
 module.exports = bench
 
 function bench(name, iterations, fn) {
   debug('setting up: "%s"', name)
-  // The length of the arguments expected defined by bench.
-  var length = this.length
-  var args = new Array(length)
-  for (var i = 0; i < length; i++) { args.push(arguments[i]) }
-  var source = benchmark.apply(null, args)
-
-  process.nextTick(function next() {
-    lazy(harness)
-  })
-
-  function harness(suite) {
+  var chunk = benchmark(name, iterations, fn)
+  setup(function onsetup(suite) {
     debug('running: "%s"', name)
-    pump(source, suite)
-  }
+    suite.write(chunk)
+  })
 }
